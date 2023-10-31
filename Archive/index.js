a9b536859a20e1fb41c79f52a897097c3a569926
BhargavNikumbh <72493359+BhargavNikumbh@users.noreply.github.com>
2023-10-31 13:31:01 +0000

    Final Version of Website
    Final Version of Website

--- Archive/index.js
+++ Archive/index.js
@@ -76,6 +76,8 @@ async function setFilterMap(party ,rank, state){
   if(state != null){
     filterMap.set("state",state);
   }
+  // displayFilteredSenatorPage();
+  // displayFilteredSenatorPage1();
 }
 
 async function displayHomePage() {
@@ -179,10 +181,8 @@ async function displaySenatorPage() {
   filter_dropdown+=optionState;
   filter_dropdown+=`
                 </select>
-                <button onclick="displayFilteredSenatorPage()">Search</button>
-                <button onclick="displaySenatorPage()">See All</button>
-              </div>
-              <p style="font-size: large;">Double click to expand and collapse options</p>`;
+                <button onclick="displayFilteredSenatorPage1()">Search</button>
+              </div>`;
   senatorConatiner += filter_dropdown; 
 
   for (let obj of senatorArray) {
@@ -222,16 +222,96 @@ async function displaySenatorPage() {
   document.getElementById("app-root").innerHTML = senatorConatiner;
 }
 
-async function displayFilteredSenatorPage(){
+async function displayFilteredSenatorPage() {
+  let senatorArray1=[];
+  let senatorConatiner = "";
+  // let n=[];
+
+  if(filterct==0){
+    senatorArray1 = data.objects;
+    filterct++;
+  }else{
+    for(let i=0;i<filteredArray.length;i++){
+      senatorArray1.push(filteredArray[i]);
+      // delete filteredArray[i];
+    }
+  }
+  filteredArray=[];
+  console.log("filtered Array"+filteredArray.length);
+
+  senatorConatiner += filter_dropdown;
+  console.log(filterMap);
+  for(let obj of senatorArray1){
+      if(obj.party == filterMap.get("party")){
+        filteredArray.push(obj); 
+      }
+      if(obj.senator_rank_label == filterMap.get("rank")){
+        filteredArray.push(obj);
+      }
+      if(obj.state == filterMap.get("state")){
+        filteredArray.push(obj);
+      }
+  }
+  senatorConatiner+=`<h3>Showing results for:
+                      <div style="display: flex;">
+                      <p>Party: ${filterMap.get("party")}</p> &emsp;
+                      <p>Rank: ${filterMap.get("rank")}</p> &emsp;
+                      <p>State: ${filterMap.get("state")}</p> &nbsp;
+                      </div></h3>`
+  filterMap.set("party",null);
+  filterMap.set("rank",null);
+  filterMap.set("state",null);
+  console.log("Senator Array"+senatorArray1.length);
+  console.log("filtered Array after for loop"+filteredArray.length);
+  if(filteredArray.length>0){
+    for (let obj of filteredArray) {
+      senatorConatiner += `   <div class="senator_container" onclick="manageCollapsableContent()">
+                              <button type="button" class="collapsible">
+                                  <h3>${
+                                    obj.person.firstname +
+                                    " " +
+                                    obj.person.lastname
+                                  }</h3>
+                                  <b>Party: &nbsp;</b>${obj.party}
+                                  <b>State: &nbsp;</b> ${obj.state}
+                                  <b>Gender: &nbsp;</b> ${obj.person.gender}
+                                  <b>Rank: &nbsp;</b> ${obj.senator_rank_label}
+                              </button>
+                              <div class="content">
+                              <b><p>Office: &nbsp;</p></b>${obj.extra.address}
+                                  
+                                  <b><p>Date of Birth: &nbsp;</p></b> ${
+                                    obj.person.birthday
+                                  }
+                                  <b><p>Start Date: &nbsp;</p></b> ${
+                                    obj.startdate
+                                  }
+                                  <b><p>Twitter: &nbsp;</p></b> <a href="${
+                                    obj.person.twitterid
+                                  }">${obj.person.twitterid}</a>
+                                  <b><p>YouTube Handle: &nbsp;</p></b> <a href="${
+                                    obj.person.youtubeid
+                                  }">${obj.person.youtubeid}</a>
+                                  <b><p>Senator Website: &nbsp;</p></b> <a href="${
+                                    obj.website
+                                  }" target="_blank">${obj.website}</a>
+                              </div>
+                          </div>`;
+    }
+  }
+  else{
+    senatorConatiner+=`<h1>Sorry, No Senators Found</h1>`
+    senatorArray1=data.objects;
+  }
+  document.getElementById("app-root").innerHTML = senatorConatiner;
+}
+
+async function displayFilteredSenatorPage1(){
   let senatorArray = data.objects;
-  let republicanArray=[];
-  let demoArray=[];
   let displayArray=[];
   let senatorConatiner="";
   senatorConatiner += filter_dropdown;
-  if(filterMap.get("party")==null && filterMap.get("state")==null && filterMap.get("rank")==null){
-    displaySenatorPage();
-  }
+  console.log(filterMap);
   for(let obj of senatorArray){
     if(filterMap.get("party")==obj.party && filterMap.get("state")==obj.state && filterMap.get("rank")==obj.senator_rank_label){
       displayArray.push(obj);
@@ -249,17 +329,6 @@ async function displayFilteredSenatorPage(){
       displayArray.push(obj);
     }
   }
-
-  for(let obj of displayArray){
-    if(obj.party=='Republican'){
-      republicanArray.push(obj);
-    }else{
-      demoArray.push(obj);
-    }
-  }
-  
-  displayArray = republicanArray.concat(demoArray);
-
   for (let obj of displayArray) {
     senatorConatiner += `   <div class="senator_container" onclick="manageCollapsableContent()">
                             <button type="button" class="collapsible">
